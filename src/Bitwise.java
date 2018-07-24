aba47e292c7450c2790a247dddd7dff516a0f99a
Jinchao Yin <jcyin.yahoo.com>
2018-07-24 20:43:27 +0800

    0724

--- src/Bitwise.java
+++ src/Bitwise.java
@@ -1,3 +1,17 @@
 public class Bitwise {
+    //No.191
+    public static int hammingWeight(int n) {
+        String binary = Integer.toBinaryString(n);
+        int weight = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary.charAt(i) == '1') {
+                weight++;
+            }
+        }
+        return weight;
+    }
 
+    public static void main(String[] args) {
+        //System.out.println(hammingWeight(11));
+    }
 }
