1b887dde7079a10313be800887a51a4d786bee16
cedarforest7 <jacquelineyin7@gmail.com>
2018-07-30 10:54:15 +0800

    updated 0730

--- src/Others3.java
+++ src/Others3.java
@@ -1,4 +1,5 @@
-
+import java.util.LinkedList;
+import java.util.List;
 
 public class Others3 {
     //No.263
@@ -16,5 +17,35 @@ public class Others3 {
             num = num/5;
         }
         return num == 1;
+    }
+
+    //No.119
+    public List<Integer> getRow1(int rowIndex) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        if (rowIndex == 0) {
+            return list;
+        }
+        List<Integer> pre = getRow1(rowIndex - 1);
+        for (int i = 1; i < rowIndex;i++) {
+            list.add(pre.get(i - 1) + pre.get(i));
+        }
+        list.add(1);
+        return list;
+    }
+
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        if (rowIndex == 0) {
+            return list;
+        }
+        long temp = rowIndex;
+        for (int i = 1; i < rowIndex;i++) {
+            list.add(Math.toIntExact(temp));
+            temp = temp*(rowIndex - i)/(i + 1);
+        }
+        list.add(1);
+        return list;
     }
 }
