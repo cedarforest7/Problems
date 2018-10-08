754f7c065d7b0916852be427e595f8441547ea29
cedarforest7 <jacquelineyin7@gmail.com>
2018-10-08 13:32:25 -0400

    updated1008

--- src/ListNode.java
+++ src/ListNode.java
@@ -330,7 +330,18 @@ public class ListNode {
         return dummy.next;
     }
 
-
+    public ListNode middleNode(ListNode head) {
+        // write your code here
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 
     public static void main(String[] args) {
 //        ListNode lx6 = new ListNode(9);
