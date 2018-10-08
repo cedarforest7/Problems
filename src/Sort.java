144df69b31e6000ba3fbbb1e00eca11108b749a5
cedarforest7 <jacquelineyin7@gmail.com>
2018-10-08 16:11:18 -0400

    1008

--- src/Sort.java
+++ src/Sort.java
@@ -18,4 +18,46 @@ public class Sort {
             nums[i] = 2;
         }
     }
+
+    public void sortIntegers2(int[] A) {
+        if (A == null) {
+            return;
+        }
+        quicksort(A, 0, A.length - 1);
+    }
+
+    //lintcode 464
+    private void quicksort (int[] A, int start, int end) {
+        // quicksort
+        if (end <= start) {
+            return;
+        }
+        //partition
+        int left = start, right = end;
+        int mid = left + (right - left) / 2;
+        int pivot = A[mid];
+        while (left <= right) {
+            while (left <= right && A[left] < pivot) {
+                //current num is at the correct position
+                left++;
+            }
+            while (left <= right && A[right] > pivot) {
+                right--;
+            }
+            //swap left and right
+            if (left <= right) {
+                swap(A, left, right);
+                left++;
+                right--;
+            }
+        }
+        quicksort(A, start, right);
+        quicksort(A, left, end);
+    }
+
+    private void swap (int[] A, int left, int right){
+        int temp = A[left];
+        A[left] = A[right];
+        A[right] = temp;
+    }
 }
