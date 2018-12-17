71ddcbc9894024f8a82f51b00ab8bdca3c4f6dfe
cedarforest7 <jacquelineyin7@gmail.com>
2018-12-16 22:21:30 -0500

    updated1216

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 
-//best sol: doubly linked list
+
 public class LRUCache {
-    Map<Integer, Integer> map;
+    /*Map<Integer, Integer> map;
     List<Integer> cache;
     //list as a FIFO queue
 
@@ -42,12 +42,106 @@ public class LRUCache {
         }
         map.put(key, value);
     }
+*/
 
+
+    class ListNode {
+        int key;
+        int val;
+        ListNode next;
+
+        public ListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+            next = null;
+        }
+    }
+
+    ListNode dummy;   //store the keys
+    Map<Integer, ListNode> map;    //store the key nodes
+    Map<Integer, ListNode> prev;     //store the previous node
+    //Map<Integer, Integer> values;
+    ListNode last;
+    int capacity;
+
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        dummy = new ListNode(-1, -1);
+        map = new HashMap<>();
+        prev = new HashMap<>();
+        last = dummy;
+        //values = new HashMap<>();
+    }
+
+    /*
+     * @param key: An integer
+     * @return: An integer
+     */
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        moveToLast(key);
+        return last.val;
+    }
+
+    /*
+     * @param key: An integer
+     * @param value: An integer
+     * @return: nothing
+     */
+    public void set(int key, int value) {
+
+        if (map.containsKey(key)) {
+            //key is already present, delete the original node
+            moveToLast(key);
+            last.val = value;
+        } else if (map.keySet().size() == capacity) {
+            //does not contain the key and reached capacity
+            //System.out.println(key);
+            ListNode first = dummy.next;
+            map.remove(first.key);
+            prev.remove(first.key);
+            first.key = key;
+            first.val = value;
+            map.put(key, first);
+            prev.put(key, dummy);
+            moveToLast(key);
+        } else {
+            //insert a new node
+            //addLastNode(key, map.get(key).val);
+            ListNode temp = new ListNode(key, value);
+            last.next = temp;
+            map.put(key, temp);
+            prev.put(key, last);
+            last = temp;
+        }
+
+
+    }
+    
+
+    private void moveToLast(int key) {
+        ListNode node = map.get(key);
+        if (last == node) {
+            return;
+        }
+        ListNode pre = prev.get(key);
+        pre.next = pre.next.next;
+        if (node.next != null) {
+            prev.put(node.next.key, pre);
+        }
+        prev.put(key, last);
+        last.next = node;
+        last = node;
+        node.next = null;
+    }
 
     public static void main(String[] args) {
         LRUCache cache = new LRUCache( 2 /* capacity */ );
 
-        cache.put(1, 1);
+        /*cache.put(1, 1);
         cache.put(2, 2);
         cache.get(1);       // returns 1
         cache.put(3, 3);    // evicts key 2
@@ -55,7 +149,17 @@ public class LRUCache {
         cache.put(4, 4);    // evicts key 1
         cache.get(1);       // returns -1 (not found)
         cache.get(3);       // returns 3
-        cache.get(4);       // returns 4
+        cache.get(4);       // returns 4*/
+
+
+        cache.set(2,1);
+        cache.set(1,1);
+        System.out.println(cache.get(2));
+        cache.set(4,1);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(2));
+
+
     }
 
 }
